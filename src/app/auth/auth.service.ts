4920850b84bd015bb7eca89da11a8d521ddbc3f3
justJosh1004 <j.joson@live.com>
2019-06-27 09:49:52 -0400

    logged out in firebase on logout click

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -37,6 +37,7 @@ export class AuthService {
   }
 
   logout() {
+    this.afAuth.auth.signOut();
     this.authChange.next(false);
     this.router.navigate(['/login']);
     this.isAuthenticated = false;
