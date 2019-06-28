f455dc21815670c7c5248c02b606a023ef2ff452
justJosh1004 <j.joson@live.com>
2019-06-28 15:26:13 -0400

    added firebase to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDrc0ARWxeJWYvTgNysFT04gzP0Miw-D7M',
+    authDomain: 'fitness-tracker-tutorial.firebaseapp.com',
+    databaseURL: 'https://fitness-tracker-tutorial.firebaseio.com',
+    projectId: 'fitness-tracker-tutorial',
+    storageBucket: 'fitness-tracker-tutorial.appspot.com',
+    messagingSenderId: '674989598087',
+    appId: '1:674989598087:web:f1b8609fb8829616'
+  }
 };
