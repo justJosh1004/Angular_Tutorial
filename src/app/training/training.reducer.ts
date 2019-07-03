ca52e85d74e09e3ee65295a7370431b3852081fa
justJosh1004 <j.joson@live.com>
2019-07-03 12:01:43 -0400

    using trainig reducer with lazy loading

--- src/app/training/training.reducer.ts
+++ src/app/training/training.reducer.ts
@@ -7,6 +7,7 @@ import {
 } from './training.actions';
 import { Exercise } from './exercise.model';
 import * as fromRoot from '../app.reducer';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 export interface TrainingState {
   availableExercises: Exercise[];
@@ -24,7 +25,7 @@ const initialState: TrainingState = {
   activeTraining: null
 };
 
-export function authReducer(state = initialState, action: TrainingActions) {
+export function trainingReducer(state = initialState, action: TrainingActions) {
   switch (action.type) {
     case SET_AVAILABLE_TRAININGS:
       return {
@@ -52,8 +53,19 @@ export function authReducer(state = initialState, action: TrainingActions) {
   }
 }
 
-export const getAvailableExercises = (state: TrainingState) =>
-  state.availableExercises;
-export const getFinishedExercises = (state: TrainingState) =>
-  state.finishedExercises;
-export const getActiveTraining = (state: TrainingState) => state.activeTraining;
+export const getTrainingState = createFeatureSelector<TrainingState>(
+  'training'
+);
+
+export const getAvailableExercises = createSelector(
+  getTrainingState,
+  (state: TrainingState) => state.availableExercises
+);
+export const getFinishedExercises = createSelector(
+  getTrainingState,
+  (state: TrainingState) => state.finishedExercises
+);
+export const getActiveTraining = createSelector(
+  getTrainingState,
+  (state: TrainingState) => state.activeTraining
+);
